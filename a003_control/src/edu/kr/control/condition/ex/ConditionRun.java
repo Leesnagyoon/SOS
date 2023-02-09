c5375e9bf55c987d216efcbe41d687c6982019ed
Yoon <dltkddbs0714@naver.com>
2023-02-09 13:52:25 +0900

    20230209_조건문if

---
+++ a003_control/src/edu/kr/control/condition/ex/ConditionRun.java
@@ -0,0 +1,38 @@
+package edu.kr.control.condition.ex;
+
+import java.util.Scanner;
+
+//실행용 클래스(Run)
+// --> 실행하고자 하는 코드를 작성한 후 Run
+public class ConditionRun {
+	public static void main(String[] args) {
+
+		// ConditionEx의 코드를 여기서 사용 가능하게 함(<-일시적 해석)
+		ConditionEx condition = new ConditionEx();
+
+		// condition.test2();
+		// condition.test1();
+		// condition.test3();
+
+		// 제어문 (코드 수행의 흐름을 제어하는 구문)
+
+		// 조건문
+		// 조건식 : 결과가 true/ false 인 식
+
+		// if문 : 만약 조건식이 (true/ false) 라면 수행하겠다
+		// if( 조건식1) {수행될 코드;}
+		// else if(조건식2) {수행될 코드;)
+		// else if(조건식3) {수행될 코드;}
+		// else {수행될 코드;} <---- 거짓이라면 수행
+
+		// if 예시 1번
+
+		//condition.ex1();
+        //condition.ex2();
+        //condition.ex3();
+        //condition.ex4();
+		condition.ex6();
+        
+	}
+
+}
